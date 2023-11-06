9930483056d2b17152d66dc16185721e634b669c
Alejandro <alejandrord3006@gmail.com>
2023-11-06 12:50:32 +0100

    Linked Login and Register activities

--- app/src/main/java/com/example/nicestart/Login.java
+++ app/src/main/java/com/example/nicestart/Login.java
@@ -5,25 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
-    private Button buttonoutline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
 
-        buttonoutline = findViewById(R.id.buttonoutline);
-
-        buttonoutline.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Login.this, Register.class);
-                startActivity(intent);
-            }
-        });
+    public void openRegister(View v) {
+        Intent intent = new Intent(Login.this, Register.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/nicestart/Register.java
+++ app/src/main/java/com/example/nicestart/Register.java
@@ -5,25 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class Register extends AppCompatActivity {
-
-    private Button buttonoutline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
 
-        buttonoutline = findViewById(R.id.buttonoutline);
-
-        buttonoutline.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Register.this, Login.class);
-                startActivity(intent);
-            }
-        });
+    public void openLogin(View v) {
+        Intent intent = new Intent(Register.this, Login.class);
+        startActivity(intent);
     }
 }
