aa360f95adc7c8ffa5f3a352950eef5fc63daee1
Alejandro <alejandrord3006@gmail.com>
2023-11-13 13:21:06 +0100

    Now using "Glide" to show high-resolution images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment:2.5.3")
     implementation("androidx.navigation:navigation-ui:2.5.3")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/nicestart/Login.java
+++ app/src/main/java/com/example/nicestart/Login.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Login extends AppCompatActivity {
 
@@ -12,6 +15,12 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ImageView mGirl = findViewById(R.id.girl);
+
+        Glide.with(this)
+                .load(R.drawable.girl)
+                .into(mGirl);
     }
 
     public void openMain(View v) {
