c3e6a2b095b7b2b150711b3d6814b37d279e737b
Alejandro <alejandrord3006@gmail.com>
2023-11-20 13:31:34 +0100

    Add WebView to Nopeople

--- app/src/main/java/com/example/nicestart/Nopeople.java
+++ app/src/main/java/com/example/nicestart/Nopeople.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Toast;
 
@@ -17,8 +18,14 @@ public class Nopeople extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nopeople);
 
-        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.myswipe); // Cast to the SwipeRefreshLayout class
-        swipeLayout.setOnRefreshListener(mOnRefreshListener); // Set listener to swipeLayout
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.myswipe); // Cast para definir el SwipeRefreshLayout
+        swipeLayout.setOnRefreshListener(mOnRefreshListener); // Agregar un listener a swipeLayout
+
+        myWebView = (WebView) findViewById(R.id.vistaweb); // Cast para definir el WebView
+        WebSettings webSettings = myWebView.getSettings(); // Declarar un WebSettings para atribuir propiedades al WebView
+        webSettings.setLoadWithOverviewMode(true); // Opciones para cargar la página ajustada al tamaño de la pantalla
+        webSettings.setUseWideViewPort(true);
+        myWebView.loadUrl("https://thispersondoesnotexist.com");
     }
 
     protected SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
@@ -26,6 +33,7 @@ public class Nopeople extends AppCompatActivity {
         public void onRefresh() {
             Toast toast0 = Toast.makeText(Nopeople.this, "Hi there! I don't exist :)", Toast.LENGTH_LONG);
             toast0.show();
+            myWebView.reload(); // Recargar el WebView
             swipeLayout.setRefreshing(false); // End the refresh, or else it'll be refreshing forever
         }
     };
