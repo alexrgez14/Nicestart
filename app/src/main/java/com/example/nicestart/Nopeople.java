40ee605622350d06c77fb66f3fd962b79b016137
Alejandro <alejandrord3006@gmail.com>
2023-12-18 13:16:00 +0100

    Add context menu to Nopeople

--- app/src/main/java/com/example/nicestart/Nopeople.java
+++ app/src/main/java/com/example/nicestart/Nopeople.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Nopeople extends AppCompatActivity {
@@ -26,6 +32,11 @@ public class Nopeople extends AppCompatActivity {
         webSettings.setLoadWithOverviewMode(true); // Opciones para cargar la página ajustada al tamaño de la pantalla
         webSettings.setUseWideViewPort(true);
         myWebView.loadUrl("https://thispersondoesnotexist.com");
+
+        // casting a la vista a la que aplicamos un menu contextual
+        // y la registramos
+        WebView mycontext = (WebView) findViewById(R.id.vistaweb);
+        registerForContextMenu(mycontext);
     }
 
     protected SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
@@ -37,4 +48,27 @@ public class Nopeople extends AppCompatActivity {
             swipeLayout.setRefreshing(false); // End the refresh, or else it'll be refreshing forever
         }
     };
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.item1) {
+            Toast toast = Toast.makeText(this,"Image copied successfully", Toast.LENGTH_LONG );
+            toast.show();
+            return true;
+        } else if (item.getItemId() == R.id.item2) {
+            Toast toast2 = Toast.makeText(this,"Image downloaded", Toast.LENGTH_LONG );
+            toast2.show();
+            return true;
+        } else {
+            return super.onContextItemSelected(item);
+        }
+    }
 }
